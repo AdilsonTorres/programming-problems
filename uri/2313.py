b834de0d3f57c040a3d64cacc8763dac6da6a589
Adilson Souza <adilsontgs@nic.br>
2018-05-16 14:29:20 -0300

    Refactor URI-2313

--- uri/2313.py
+++ uri/2313.py
@@ -1,15 +1,18 @@
 a, b, c = map(int, input().split(' '))
 
+valid = True
 if a + b <= c or a + c <= b or b + c < a:
     print('Invalido')
+    valid = False
 elif a == b and b == c:
     print("Valido-Equilatero")
 elif a == b and a != c or a == c and a != b or b == c and b != a:
     print("Valido-Isoceles")
 else:
-    print("Valido-escaleno")
+    print("Valido-Escaleno")
 
-if a*a + b*b == c*c or a*a + c*c == b*b or c*c + b*b == a*a:
-    print("Retangulo: S")
-else:
-    print("Retangulo: N")
+if valid:
+    if a*a + b*b == c*c or a*a + c*c == b*b or c*c + b*b == a*a:
+        print("Retangulo: S")
+    else:
+        print("Retangulo: N")
